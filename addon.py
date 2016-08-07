96b954fa1568769fe4e0cad731f41409cdeeaab3
Dominik Schwarz <Dominik Schwarz>
2016-08-07 17:38:18 +0200

    add DHT11 data parsing and display temperature and humidity

--- addon.py
+++ addon.py
@@ -1,11 +1,80 @@
+import sys
+sys.path.append('/storage/.kodi/addons/python.RPi.GPIO/lib')
 import xbmcaddon
 import xbmcgui
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
- 
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
- 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+import RPi.GPIO as GPIO
+import time
+
+def bin2dec(string_num):
+    return str(int(string_num, 2))
+
+GPIO.setmode(GPIO.BOARD)
+
+starttime = time.time()
+while (time.time()-starttime) < 5.0:
+	try:
+		data = []
+
+		GPIO.setup(5,GPIO.OUT)
+		GPIO.output(5,GPIO.HIGH)
+		time.sleep(0.025)
+		GPIO.output(5,GPIO.LOW)
+		time.sleep(0.02)
+
+		GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		for i in range(0,1000):
+			data.append(GPIO.input(5))
+
+		bit_count = 0
+		count = 0
+		HumidityBit = ""
+		TemperatureBit = ""
+		crc = ""
+
+		while data[count] == 1:
+			count = count + 1
+		
+		for i in range(0, 40):
+			bit_count = 0
+
+			while data[count] == 0:
+				count = count + 1
+
+			while data[count] == 1:
+				bit_count = bit_count + 1
+				count = count + 1
+
+			if bit_count > 8:
+				if i>=0 and i<8:
+					HumidityBit = HumidityBit + "1"
+				if i>=16 and i<24:
+					TemperatureBit = TemperatureBit + "1"
+				if i>=32 and i<40:
+					crc = crc + "1"
+			else:
+				if i>=0 and i<8:
+					HumidityBit = HumidityBit + "0"
+				if i>=16 and i<24:
+					TemperatureBit = TemperatureBit + "0"
+				if i>=32 and i<40:
+					crc = crc + "0"
+	   
+		Humidity = bin2dec(HumidityBit)
+		Temperature = bin2dec(TemperatureBit)
+		
+		if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
+			break
+	except:
+		continue		
+		
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+
+hello = "Hello Guys!"
+if((time.time()-starttime) < 5.0):
+	line2 = "Temperature: "+Temperature+"C"
+	line3 = "Humidity: "+Humidity+"%"
+else:
+	line2 = "OMG, this is so embarrassing :("
+	line3 = "I can't read the data!!!"
+xbmcgui.Dialog().ok(addonname, hello, line2, line3)
